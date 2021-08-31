b58da6512d15ca7394615d8edff846692a726c0f
Justin <jusglinton@gmail.com>
2021-08-31 05:01:52 -0400

    first commit

---
+++ src/scenes/GameScene.js
@@ -0,0 +1,16 @@
+import Phaser  from "phaser";
+
+export default class GamesScene extends Phaser.Scene{
+
+    constructor(){
+        super('game-scene')
+    }
+
+    preloade(){
+
+    }
+
+    create(){
+        
+    }
+}
